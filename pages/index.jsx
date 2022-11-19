31a7d6e5b27dc7e89ab3b0e38a204479c6986ba1
Max Alejandro <max.flores.ar@gmail.com>
2022-11-19 10:30:29 -0600

    yes

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,37 +4,41 @@ import Image from "next/image";
 import Link from "next/link";
 
 import image from "../public/assets/images/chipa-bot.png";
-import ChipaButton from "../src/common/containers/components/Button.jsx"
+import ChipaButton from "../src/common/containers/components/Button.jsx";
 import Context from "../src/common/hooks/Context";
 
-const Slogan = ({lang}) => (
-  <section className="slogan">
-    <Image src={image} alt="chipa-bot" />
-    <h2 className="title">{ lang.title }</h2>
-    <p className="text">{ lang.slogan }</p>
+import styles from "../public/styles/pages/index.module.scss";
+
+const Slogan = ({ lang }) => (
+  <section className={styles.slogan}>
+    <Image className={styles.logo} src={image} alt="chipa-bot" />
+    <div className={styles.info}>
+      <h2 className="title">{lang.title}</h2>
+      <p className="text">{lang.slogan}</p>
+    </div>
   </section>
 );
 
-const Add = ({lang}) => (
-  <section className="add">
-    <h3 className="title">{ lang.add.title }</h3>
-    <ChipaButton>
-      <Link href={"/login"}>{ lang.add.button }</Link>
+const Add = ({ lang }) => (
+  <section className={styles.add}>
+    <h3 className="title">{lang.add.title}</h3>
+    <ChipaButton isDark={true}>
+      <Link href={"/login"}>{lang.add.button}</Link>
     </ChipaButton>
   </section>
-)
+);
 
 const App = () => {
   const { theme, stateLang } = Context();
-  const { landing } = stateLang
+  const { landing } = stateLang;
   return (
     <>
       <Head>
-        <title>{ landing.title }</title>
+        <title>{landing.title}</title>
       </Head>
       <div className={`${theme} app`}>
-        <Slogan lang={landing}/>
-        <Add lang={landing}/>
+        <Slogan lang={landing} />
+        <Add lang={landing} />
       </div>
     </>
   );
